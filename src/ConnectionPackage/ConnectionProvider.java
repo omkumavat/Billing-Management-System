785454c0fe29eb36198373c0eab64e4b13e3099c
omkumavat <omkumavat2004@gmail.com>
2025-06-14 17:56:46 +0530

    Done

---
+++ src/ConnectionPackage/ConnectionProvider.java
@@ -0,0 +1,24 @@
+package ConnectionPackage;
+
+import java.sql.*;
+
+public class ConnectionProvider {
+    public static Connection getcon() {
+        try {
+            // Load MySQL JDBC Driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Establish Connection
+            Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/db_billing?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC",
+//                    "jdbc:mysql://localhost:3306/db_billing?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC",
+                    "root",
+                    "Divya424101@"
+            );
+            return con;
+        } catch (Exception e) {
+            e.printStackTrace(); // Print error to console
+            return null;
+        }
+    }
+}
